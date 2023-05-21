1b6f32b886c69313d5cdd13760a7271331f96da6
Alice <000>
2023-05-21 21:53:36 +0300

    function add recs

--- src/api/favBooks.js
+++ src/api/favBooks.js
@@ -1,7 +1,28 @@
 import { getAll } from "./books";
+import users from "./users"
 
 export const books = await getAll();
 export const favBooks = [];
+export const recBooks = [];
+
+function showRecs(id) {
+    for(let user in users) {
+        const favourite = users[user].favBooks;
+        const match = favourite.some((e) => e.id == id)
+        if(match) {
+            favourite.forEach(e => {
+                if(recBooks.includes(e)) {
+                    const index = recBooks.indexOf(e);
+                    recBooks.splice(index, 1);
+                } else{
+                    recBooks.push(e);
+                }
+            })
+        }
+    }
+    console.log(recBooks);
+  }
+
 
 export function addFav(id) {
     const book = books.find(book => book.id === id);
@@ -12,4 +33,5 @@ export function addFav(id) {
       favBooks.push(book);
     }
     console.log(favBooks);
+    showRecs(id);
   }
---
+++ src/api/users.js
@@ -0,0 +1,73 @@
+const users = [
+    {
+      id: 1,
+      favBooks: [
+        {
+            id: 1,
+            name: "Перемените обстановку",
+            author: "Джеймс Хедли Чейз",
+            img: "https://knigopoisk.org/media/books/55/553425d52eb5d.jpg",
+        },
+        {
+            id: 4,
+            name: "Убийство на улице Морг",
+            author: "Эдгар Аллан По",
+            img: "https://knigopoisk.org/media/books/ub/ubiystvo-na-ulice-morg--68433.jpg",
+        },
+        {
+            id: 5,
+            name: "Записки о Шерлоке Холмсе",
+            author: "Артур Конан Дойл",
+            img: "https://knigopoisk.org/media/books/55/5564a89be3600.jpg",
+        },
+      ]
+    },
+    {
+        id: 2,
+        favBooks: [
+            {
+                id: 2,
+                name: "Легион. Ложь и тайна",
+                author: "Дэн Абнетт",
+                img: "https://knigopoisk.org/media/books/le/legion-lozh-i-tayna--62459.jpg",
+            },
+            {
+                id: 6,
+                name: "Падение Гипериона",
+                author: "Дэн Симмонс",
+                img: "https://knigopoisk.org/media/books/pa/padenie-giperiona--97357.jpg",
+            },
+            {
+                id: 7,
+                name: "Тысяча сынов",
+                author: "Грэм Макнилл",
+                img: "https://knigopoisk.org/media/books/ty/tysyacha-synov--38094.jpg",
+            }
+        ]
+      },
+      {
+        id: 3,
+        favBooks: [
+            {
+                id: 3,
+                name: "Племя тьмы",
+                author: "Клайв Баркер",
+                img: "https://knigopoisk.org/media/books/pl/plemya-t-my--82224.jpg",
+            },
+            {
+                id: 8,
+                name: "Сияние",
+                author: "Стивен Кинг",
+                img: "https://knigopoisk.org/media/books/St/Stiven_King__Siyanie.jpg",
+            },
+            {
+                id: 9,
+                name: "Хребты безумия",
+                author: "Говард Филлипс Лавкрафт",
+                img: "https://knigopoisk.org/media/books/hr/hrebty-bezumiya--97780.jpg",
+            }
+        ]
+      }
+  ];
+
+  export default users;
