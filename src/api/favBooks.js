175f219eb901585cc6af8827cd300834cd275904
solarburst <ausesin@gmail.com>
2023-05-29 23:24:29 +0700

    refactoring favourite page

--- src/api/favBooks.js
+++ src/api/favBooks.js
@@ -1,37 +1,38 @@
 import { getAll } from "./books";
-import users from "./users"
+import users from "./users";
 
 export const books = await getAll();
 export const favBooks = [];
 export const recBooks = [];
 
 function showRecs(id) {
-    for(let user in users) {
-        const favourite = users[user].favBooks;
-        const match = favourite.some((e) => e.id == id)
-        if(match) {
-            favourite.forEach(e => {
-                if(recBooks.includes(e)) {
-                    const index = recBooks.indexOf(e);
-                    recBooks.splice(index, 1);
-                } else{
-                    recBooks.push(e);
-                }
-            })
+  for (let user in users) {
+    const favourite = users[user].favBooks;
+    const match = favourite.some((e) => e.id == id);
+    if (match) {
+      favourite.forEach((e) => {
+        if (recBooks.includes(e)) {
+          const index = recBooks.indexOf(e);
+          recBooks.splice(index, 1);
+        } else {
+          recBooks.push(e);
         }
+      });
     }
-    console.log(recBooks);
   }
-
+  console.log(recBooks);
+}
 
 export function addFav(id) {
-    const book = books.find(book => book.id === id);
-    if(favBooks.includes(book)) {
-      const index = favBooks.indexOf(book);
-      favBooks.splice(index, 1);
-    } else {
-      favBooks.push(book);
-    }
-    console.log(favBooks);
-    showRecs(id);
+  const book = books.find((book) => book.id === id);
+  if (favBooks.includes(book)) {
+    book.isFavourite = false;
+    const index = favBooks.indexOf(book);
+    favBooks.splice(index, 1);
+  } else {
+    book.isFavourite = true;
+    favBooks.push(book);
   }
+  console.log("fav books in api", favBooks);
+  showRecs(id);
+}
